f90844c1052fcd517e98238a82daa50fecd600f2
Jorge Perez <me@juniorjorge.com>
2021-03-01 22:42:57 -0500

    Add tests for each if statement

--- solutions/1.2.py
+++ solutions/1.2.py
@@ -3,16 +3,24 @@
 
 def CheckPermutation(t_one, t_two):
     if len(t_one) != len(t_two):
+        print('Fail Length')
         return False
     if t_one == t_two:
+        print('Fail same string')
         return False
     if set(t_one) != set(t_two):
+        print('Fail set')
         return False
     if sorted(t_one) != sorted(t_two):
+        print('Fail sort')
         return False
     return True
 
 
 if __name__ == '__main__':
+    print(CheckPermutation('apple', 'apples'))
+    print(CheckPermutation('apple', 'apple'))
+    print(CheckPermutation('apple', 'applo'))
+    print(CheckPermutation('apale', 'eppla'))
+
     print(CheckPermutation('apple', 'aplpe'))
-    print(CheckPermutation('apple', 'alape'))
